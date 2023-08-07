a29194167fb1f727506a732019e79f9c469c5e24
birdslisten <justin.schultz37@gmail.com>
2023-08-07 15:04:26 -0700

    added generatePassword Function, and Promt for PW length

--- script.js
+++ script.js
@@ -1,4 +1,30 @@
 // Assignment code here
+function generatePassword() {
+  var pass = '';
+  var str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' +
+    'abcdefghijklmnopqrstuvwxyz' + '0123456789@#$';
+
+  for (let i = 1; i <= promptLength; i++) {
+    var char = Math.floor(Math.random()
+      * str.length + 1);
+
+    pass += str.charAt(char)
+  }
+
+  return pass;
+}
+function promptLength() {
+  var pwlength = prompt('Please enter a number between 8 and 128 to set your password length: ');
+
+  if (pwlength >= 8 && pwlength <= 128) {
+    alert("Your chosen password length is " + pwlength + '.');
+    console.log(pwlength);
+  } else {
+    alert('Please try again');
+  }
+
+  return pwlength;
+}
 
 
 // Get references to the #generate element
@@ -14,4 +40,4 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", promptLength);
